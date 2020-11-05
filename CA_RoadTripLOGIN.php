6495d4a75c862531c50867b08d33d97e9d2e983b
kemccorm6 <63037159+kemccorm6@users.noreply.github.com>
2020-11-05 02:01:55 -0800

    Log in Form 1 Attempt

--- CA_RoadTripLOGIN.php
+++ CA_RoadTripLOGIN.php
@@ -1,2 +1,11 @@
-<?php
-hi
+Log In
+<br>
+New here? Create an Account
+
+<form action="">
+    Username: <br>
+    <input type="text" name="username" placeholder="Type Username"><br>
+    Password: <br>
+    <input type="text" name="password" placeholder="Type Password"><br>
+    <input type="submit" value="Log In">
+</form>
