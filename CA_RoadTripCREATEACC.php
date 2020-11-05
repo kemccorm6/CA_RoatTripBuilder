1bd6dabe4a996c96a0340661414218771375f686
kemccorm6 <63037159+kemccorm6@users.noreply.github.com>
2020-11-05 02:15:36 -0800

    PHP start for log in pages

--- CA_RoadTripCREATEACC.php
+++ CA_RoadTripCREATEACC.php
@@ -1,2 +1,13 @@
-<?php
-test
+Create an Account
+<br>
+Have an Account? <a href="CA_RoadTripLOGIN.php">Log In</a>
+
+<form action="">
+    Username: <br>
+    <input type="text" name="username" placeholder="Type Username"><br>
+    Password: <br>
+    <input type="text" name="password" placeholder="Type Password"><br>
+    Email:<br>
+    <input type="text" name="email" placeholder="Type Email"><br>
+    <input type="submit" value="Log In">
+</form>
--- CA_RoadTripLOGIN.php
+++ CA_RoadTripLOGIN.php
@@ -1,11 +1,25 @@
 Log In
 <br>
-New here? <a href="">Create an Account</a>
+New here? <a href="CA_RoadTripCREATEACC.php">Create an Account</a>
 
 <form action="">
     Username: <br>
     <input type="text" name="username" placeholder="Type Username"><br>
     Password: <br>
     <input type="text" name="password" placeholder="Type Password"><br>
+    <input type="checkbox" name="admin" value="admin" id="admincheck" onclick="reveal()"> Check if Admin <br>
+    <input type="text" name="adminpassword" placeholder="Type Admin Password" style="display:none" id="checkedadmin">
     <input type="submit" value="Log In">
+
+    <script>
+        function reveal(){
+            var checkbox = document.getElementById("admincheck");
+            var text = document.getElementById("checkedadmin");
+            if (checkbox.checked == true){
+                text.style.display = "block";
+            }else{
+                text.style.display = "none";
+            }
+        }
+    </script>
 </form>
