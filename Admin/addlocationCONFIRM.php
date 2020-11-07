4b34de07aee597a4ae5a5fb14ff0ef9225c8c36d
kemccorm6 <63037159+kemccorm6@users.noreply.github.com>
2020-11-06 19:33:58 -0800

    Add Location Page and COnfirm Start

---
+++ Admin/addlocationCONFIRM.php
@@ -0,0 +1,24 @@
+<?php
+
+$host = "webdev.iyaclasses.com";
+$userid = "kemccorm";
+$userpw = "Acad276_McCormick_2109860012";
+$db = "kemccorm_roadtripbuilder";
+
+
+$mysql = new mysqli(
+    $host,
+    $userid,
+    $userpw,
+    $db
+);
+
+if ($mysql->connect_errno) {
+    echo "db connection error : " . $mysql->connect_error;
+    exit();
+}
+
+$sql=
+
+?>
+
