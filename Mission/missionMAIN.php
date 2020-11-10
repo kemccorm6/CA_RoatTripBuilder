77869dec980dce901689b9c9d3424bf1d79830ce
mgdavis <63075804+mgdavis@users.noreply.github.com>
2020-11-10 09:49:33 -0800

    missionMAIN.php updates

--- Mission/missionMAIN.php
+++ Mission/missionMAIN.php
@@ -35,15 +35,22 @@ if ($mysql->connect_errno) {
     background: -webkit-linear-gradient(to bottom, #f6c157, #f9d793);  /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(to bottom, #f9d793, #f6c157); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     margin: auto;
-    width: 40%;
+    width: 50%;
     border-radius: 7px;
-    padding-left: 30px;
-    padding-right: 30px;
+    padding-left: 50px;
+    padding-right: 50px;
     padding-bottom: 40px;
     padding-top: 10px;
-    font-size: 16pt;
+    font-size: 18pt;
     box-shadow: 5px 5px 10px black;
 }
+.missionlogobox {
+    width: 40%;
+    margin: auto;
+}
+        .missionlogo {
+            width: 100%;
+        }
 .missionbox h1 {
     text-align: center;
 }
@@ -63,11 +70,13 @@ if ($mysql->connect_errno) {
         <div class="navitem"><br><br><a href="http://webdev.iyaclasses.com/~eglover/CA_RoatTripBuilder/Mission/missionMAIN.php">OUR MISSION</a> </div>
         <div class="navitem"><br><br><a href="http://webdev.iyaclasses.com/~eglover/CA_RoatTripBuilder/Team/teamMAIN.php">OUR TEAM</a> </div>
     </div>
+    <div class="profile"><a href="http://webdev.iyaclasses.com/~eglover/CA_RoatTripBuilder/Login/CA_RoadTripLOGIN.php"><img class="profileimage" src="myprofile_button-08.png"></a></div>
 </div>
-<hr>
+<!--<hr>--><br>
 <div class="container">
-
-    <div class="missionbox">California Dreamin’ is volunteer based platform bringing you all of the best road trip stops up the California coast. Our mission is to bring together adventure enthusiasts through a simple one-stop platform. Head over to “Make a Trip” to begin planning your excursion, browse through photos posted by other users, and publish your adventures for others to see!
+<div class="missionlogobox"><div class="missionlogo"><img class="missionlogo" src="ourmission_logo-07.png"></div></div>
+    <br><br>
+    <div class="missionbox">California Dreamin’ is a volunteer based platform bringing you all of the best road trip stops up the California coast. Our mission is to bring adventure enthusiasts together through a simple one-stop platform. Head over to “Make a Trip” to begin planning your excursion, browse through photos posted by other users, and publish your adventures for others to see!
         <br><br>
         Because it’s not just a trip, it’s a story to share!</div>
 
